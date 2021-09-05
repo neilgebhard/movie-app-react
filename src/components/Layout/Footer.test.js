030284424cb577df5feb0fd4d41acdf25d21a9e3
Neil Gebhard <neilgebhard@gmail.com>
2021-09-05 17:28:16 +1000

    Fix bug

--- src/components/Layout/Footer.test.js
+++ src/components/Layout/Footer.test.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import { render, screen } from "@testing-library/react";
 import Footer from "./Footer";
- 
-describe('Footer', () => {
-  test('renders Footer component', () => {
+
+describe("Footer", () => {
+  test("renders Footer component", () => {
     render(<Footer />);
     expect(screen.getByText(/Neil's Movie App/)).toBeInTheDocument();
   });
