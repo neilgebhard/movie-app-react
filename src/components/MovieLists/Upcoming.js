aeb3a57234c39e6daad9672005b16cfe2f1936c8
Neil Gebhard <neilgebhard@gmail.com>
2021-09-16 07:05:31 +1000

    Fix small bug

--- src/components/MovieLists/Upcoming.js
+++ src/components/MovieLists/Upcoming.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import MovieList from "../common/MovieList";
+import MovieList from "../Common/MovieList";
 import axios from "axios";
 
 const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
