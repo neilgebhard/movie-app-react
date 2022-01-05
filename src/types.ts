27a2de7f789ffde783dd85abdb11d89b03dae6a4
Neil Gebhard <neilgebhard@gmail.com>
2022-01-05 22:55:08 +1000

    :label: Add Movie Details type

--- src/types.ts
+++ src/types.ts
@@ -28,3 +28,49 @@ export type Movie = {
   vote_average: number;
   vote_count: number;
 };
+
+export type MovieDetails = {
+  adult: boolean;
+  backdrop_path: string;
+  belongs_to_collection: null;
+  budget: number;
+  genres: Genre[];
+  homepage: string;
+  id: number;
+  imdb_id: string;
+  original_language: string;
+  original_title: string;
+  overview: string;
+  popularity: number;
+  poster_path: string;
+  production_companies: ProductionCompanies[];
+  production_countries: ProductionCountries[];
+  release_date: string;
+  revenue: number;
+  runtime: number;
+  spoken_languages: SpokenLanguages[];
+  status: string;
+  tagline: string;
+  title: string;
+  video: boolean;
+  vote_average: number;
+  vote_count: number;
+};
+
+type Genre = {
+  id: number;
+  name: string;
+};
+export type ProductionCompanies = {
+  id: number;
+  logo_path: string;
+  name: string;
+  origin_country: string;
+};
+export type ProductionCountries = {
+  name: string;
+};
+export type SpokenLanguages = {
+  english_name: string;
+  name: string;
+};
